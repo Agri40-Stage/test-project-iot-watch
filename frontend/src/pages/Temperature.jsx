d3c451c26bfffe3f4810747cee675dbb8d8ed802
oelaimar <oussama.el.aimar@gmail.com>
2026-01-03 18:05:19 +0100

    feat: Add Footer component to Temperature page

--- frontend/src/pages/Temperature.jsx
+++ frontend/src/pages/Temperature.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 /* Components */
 import Content from '../components/Content';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 // import TemperaturePrediction from '../components/TemperaturePrediction';
 // import WeeklyStats from '../components/WeeklyStats';
 
@@ -11,6 +12,7 @@ function Temperature(){
         <div className="w-screen max-w-screen min-h-screen bg-zinc-50">
       <Header />
       <Content />
+      <Footer />  
       {/* <TemperaturePrediction />
       <WeeklyStats /> */}
     </div>
