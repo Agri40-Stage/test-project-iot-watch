bcfb09748b6fa6df3c98e74d4ef54b73b634b439
anouar <anouarechcharai@gmail.com>
2026-01-30 16:52:04 +0100

    feat: redesign dashboard with real-time weather monitoring UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
 
 createRoot(document.getElementById('root')).render(
-  <Router >
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>
 )
